3f8213fe9e64269cc0a98bcf9f6d242549f502b1
Steve Sheets <magesteve@mac.com>
2025-08-24 11:44:54 -0400

    Update

--- APGExample/AppDelegate.swift
+++ APGExample/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import Cocoa
 import APGIntentKit
+import APGWorkKit
 
 var gMyFlag = false
 
@@ -14,59 +15,80 @@ var gMyFlag = false
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    // Designated initializer
     override init() {
         super.init()
 
-        // Setup the Intent Information
+        // Setup Intents Info
         
         APGIntentInfoList.shared.add(contentsOf: [
-            APGIntentInfo(token: "myAbout",   name: "About Example",   symbolName: "info.circle"),
             APGIntentInfo(token: "myWelcome", name: "Welcome", symbolName: "sun.max"),
             APGIntentInfo(token: "myHelp",    name: "Additional Help",    symbolName: "hand.wave"),
             APGIntentInfo(token: "myDetails", name: "Details", symbolName: "macwindow"),
             
             APGIntentInfo(token: "myFlag",    name: "Flag",    alwaysOn: false, symbolName: "flag")
         ])
+
+        // Setup Work
+        
+        APGWorkMacApp.appInit()
+        
+        APGWorkGlobals.shared.workUIColor = .green
+        APGWorkGlobals.shared.addAdditionalMenuIntents(about: ["myDetails"], help: ["myHelp"])
+
+        APGWorkGlobals.shared.aboutAcknowledgmentsLink = "https://example.com"
+        APGWorkGlobals.shared.aboutLicensesLink = "https://example.com"
+        
+        APGWorkGlobals.shared.featuresList.append(APGWorkFeatureItem(title: "Detail 1",
+                                                              description: "This is more details!",
+                                                             symbolName: "star.fill"))
+        APGWorkGlobals.shared.featuresList.append(APGWorkFeatureItem(title: "Detail 2",
+                                                              description: "This is more details!",
+                                                              symbolName: "pencil"))
+        APGWorkGlobals.shared.featuresList.append(APGWorkFeatureItem(title: "Detail 3",
+                                                              description: "This is more details!",
+                                                              symbolName: "gearshape.fill"))
+        APGWorkGlobals.shared.featuresListLink = "https://example.com"
     }
 
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
-        // Setup the Intent Actions
-        
-        APGIntentActionList.sharedApp.addAction(token: "myAbout") { _ in
-            self.myAlert("This is specialized about window for the App")
-        }
-        APGIntentActionList.sharedApp.addAction(token: "myWelcome") { _ in
-            self.myAlert("This is specialized welcome window for the app")
-        }
-        APGIntentActionList.sharedApp.addAction(token: "myHelp") { _ in
-            self.myAlert("This is additional help window for the app")
-        }
-
-        APGIntentActionList.sharedApp.addAction(token: "flag",
-                                                action: { _ in gMyFlag = !gMyFlag },
-                                                appearance: { _ in return (true, gMyFlag, nil) })
-
-        // Place Intent in Menus
-        
-        APGIntentMacTools.addAppMenuIntents(about: ["myAbout", "myWelcome"],
-                                              settings: [ "myDetails"])
-        
-        APGIntentMacTools.addMenuBeforeHelp(named: "Tools", tokens: ["myFlag"])
-
-        APGIntentMacTools.addHelpMenuIntents(help: ["myHelp"])
-    }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
+    /// Simple alert to show message
     func myAlert(_ text: String) {
         let alert = NSAlert()
         alert.messageText = text
         alert.addButton(withTitle: "OK")
         alert.runModal()
     }
+
+    // Setup UI
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        // Setup the Intent Actions
+        
+        APGIntentActionList.sharedApp.addAction(token: "myWelcome") { _ in
+            self.myAlert("This is specialized welcome window for the app")
+        }
+        APGIntentActionList.sharedApp.addAction(token: "myHelp") { _ in
+            self.myAlert("This is additional help window for the app")
+        }
+        APGIntentActionList.sharedApp.addAction(token: "myDetails") { _ in
+            self.myAlert("This is additional Detail window for the app")
+        }
+        APGIntentActionList.sharedApp.addAction(token: "myFlag",
+                                                action: { _ in gMyFlag = !gMyFlag },
+                                                appearance: { _ in return (true, gMyFlag, nil) })
+
+        // Place Intent in Menus
+        
+        APGIntentMacTools.addMenuBeforeHelp(named: "Tools", tokens: ["myFlag"])
+
+        // Place Work in Menus
+        
+        APGWorkMacApp.appPrepare()
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
 }
 
--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Source examples for the various APGKit, Swift Packages for Apple Development.
 A lightweight Swift package for standardizing UI commands (menu items, toolbar items, buttons) around a simple Intent model.
 
 [Repository](https://github.com/magesteve/APGIntentKit)
+
+## APGIntentKit
+
+A SwiftUI package for creating professional MacOS applications in the style of AppleWorks.
+
+[Repository](https://github.com/magesteve/APGWorkKit)
