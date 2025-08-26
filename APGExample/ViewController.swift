643dd1bbad02f9e80791bc714372c8679c5f868e
Steve Sheets <magesteve@mac.com>
2025-08-26 02:16:09 -0400

    Update

--- APGExample/ViewController.swift
+++ APGExample/ViewController.swift
@@ -7,17 +7,34 @@
 
 import Cocoa
 import APGCantripKit
+import APGMacWidgetKit
 
+/// View Controller for the document
+/// It uses Cantrip kit to make some rich text, and then displays it using WidgetKit.
 class ViewController: NSViewController {
     
-    var text = AttributedString()
     private let richTextView = APGMacWidgetRichTextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        text.cantripH1("Example Code")
-        text.cantripP1("This is a long body of example text to demonstrate scrolling. Add as much content as you like here—paragraphs, code samples, etc")
+        var text = AttributedString()
+        text.cantripH1("APG Example")
+        text.cantripP("This sample document demonstrates many of the features of APG Kits.")
+        
+        text.cantripLine(width: 300.0, color: .black)
+        text.cantripParagraphEnd()
+        text.cantripImage(named:"steve")
+        text.cantripParagraphEnd()
+        text.cantripLine(width: 300.0, color: .black)
+        text.cantripParagraphEnd()
+
+        text.cantripH5("Sample Test to demonstrate scrolling.")
+        text.cantripP("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales, quam in facilisis scelerisque, orci sapien euismod nulla, at efficitur magna mauris vitae magna. Donec nec lorem vel velit ultricies efficitur non nec nisl. Cras viverra, nisi non feugiat malesuada, urna felis fermentum nisl, eget ultrices ipsum mauris vitae tortor. Curabitur vestibulum orci vitae odio sagittis varius. ")
+        text.cantripP("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales, quam in facilisis scelerisque, orci sapien euismod nulla, at efficitur magna mauris vitae magna. Donec nec lorem vel velit ultricies efficitur non nec nisl. Cras viverra, nisi non feugiat malesuada, urna felis fermentum nisl, eget ultrices ipsum mauris vitae tortor. Curabitur vestibulum orci vitae odio sagittis varius. ")
+        text.cantripP("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales, quam in facilisis scelerisque, orci sapien euismod nulla, at efficitur magna mauris vitae magna. Donec nec lorem vel velit ultricies efficitur non nec nisl. Cras viverra, nisi non feugiat malesuada, urna felis fermentum nisl, eget ultrices ipsum mauris vitae tortor. Curabitur vestibulum orci vitae odio sagittis varius. ")
+        text.cantripP("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales, quam in facilisis scelerisque, orci sapien euismod nulla, at efficitur magna mauris vitae magna. Donec nec lorem vel velit ultricies efficitur non nec nisl. Cras viverra, nisi non feugiat malesuada, urna felis fermentum nisl, eget ultrices ipsum mauris vitae tortor. Curabitur vestibulum orci vitae odio sagittis varius. ")
+        text.cantripP("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales, quam in facilisis scelerisque, orci sapien euismod nulla, at efficitur magna mauris vitae magna. Donec nec lorem vel velit ultricies efficitur non nec nisl. Cras viverra, nisi non feugiat malesuada, urna felis fermentum nisl, eget ultrices ipsum mauris vitae tortor. Curabitur vestibulum orci vitae odio sagittis varius. ")
 
         richTextView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(richTextView)
@@ -28,15 +45,13 @@ class ViewController: NSViewController {
             richTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
 
-        // Push content into the widget
         richTextView.setAttributedText(text)
     }
 
     override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+        didSet { }
     }
 
 }
+
 
--- README.md
+++ README.md
@@ -11,11 +11,19 @@ A SwiftUI package for creating professional MacOS applications in the style of A
 
 [Repository](https://github.com/magesteve/APGWorkKit)
 
+
 ## APGIntentKit
 
 A lightweight Swift package for standardizing UI commands (menu items, toolbar items, buttons) around a simple Intent model.
 
 [Repository](https://github.com/magesteve/APGIntentKit)
+
+
+## APGMacWidgetKit
+
+A Swift package for containing various useful MacOS windows, views and other widgets.
+
+[Repository](https://github.com/magesteve/APGMacWidgetKit)
 
 
 ## APGCantripKit
